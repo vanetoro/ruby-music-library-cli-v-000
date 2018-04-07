00c8cb9ea069c452251374ad3d47f027ba817ae9
Vanessa Toro <vanetoro84@gmail.com>
2018-04-07 16:32:20 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -16,12 +16,10 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
-    answer = nil
-    
-    
       answer = gets
       case answer
-       when 
+       when 'list_songs'
+         self.list_songs
     end  
   end
   
