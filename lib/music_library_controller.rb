5cc8d2728c47dd0d441e413ccf3f20e7e6553357
Vanessa Toro <vanetoro84@gmail.com>
2018-04-07 16:36:39 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -7,6 +7,9 @@ class MusicLibraryController
   end
   
   def call
+    answer = nil
+    while answer != 'exit'
+      answer = gets
     puts "Welcome to your music library!"
     puts "To list all of your songs, enter 'list songs'."
     puts "To list all of the artists in your library, enter 'list artists'."
@@ -16,13 +19,8 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
-    answer = nil
-    while answer != 'exit'
-      answer = gets
-    end
     
-    answer = gets
-      case answer
+    case answer
        when 'list_songs'
          self.list_songs
       end  
